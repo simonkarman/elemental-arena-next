36ad2df0d826f73945a8a93e8510f0251a6f7263
Simon Karman <mail@simonkarman.nl>
2022-04-18 15:07:13 +0200

    Added export functionality to publish the project on karman.dev

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
+// eslint-disable-next-line no-process-env
+const basePath = process.env.BASE_PATH;
 /** @type {import('next').NextConfig} */
 module.exports = {
+  basePath,
   reactStrictMode: true,
   trailingSlash: true,
   experimental: {
